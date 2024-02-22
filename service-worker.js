88a0c773a9af698f7fbce1a7c07a208a894714fa
Wilkor <wilkorguitarrista@msn.com>
2024-02-22 14:12:10 -0300

    fix
    adicionando mensagem para quando o cliente não esta em atendimento humano

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a16e855af474fbe7f7f9e8b6d0dd0bd3.js"
+  "./precache-manifest.85f4665037467a041eb9c7c657996ada.js"
 );
 
 self.addEventListener('message', (event) => {
